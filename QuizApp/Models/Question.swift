f23c4ff18b71a68d5069a15acd57ed0b5adef464
Brody Roberts <brodyroberts2002@gmail.com>
2024-08-22 15:14:42 -0700

    Had to restart project to to unforseen xcode problems

---
+++ QuizApp/Models/Question.swift
@@ -0,0 +1,58 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Brody on 8/22/24.
+//
+
+
+import Foundation
+
+struct Question: Hashable {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "Who is the GOAT of Basketball",
+                 possibleAnswers: [
+                    "LeBron James",
+                    "Stephen Curry",
+                    "Micheal Jordan",
+                    "Timofey Mozgov"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What's my dogs name?",
+                 possibleAnswers: [
+                    "Rex",
+                    "Scruffy",
+                    "Buddy",
+                    "Appa"
+                 ],
+                 correctAnswerIndex: 1),
+        Question(questionText: "Goat programming language",
+                 possibleAnswers: [
+                    "Python",
+                    "Swift",
+                    "C++",
+                    "GoLang"
+                 ],
+                 correctAnswerIndex: 0),
+        Question(questionText: "Best Anime?",
+                 possibleAnswers: [
+                    "Jujutsu Kaisen",
+                    "Naruto",
+                    "Hajime no Ippo",
+                    "One Piece"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What's my Favorite Number?",
+                 possibleAnswers: [
+                    "4",
+                    "7",
+                    "11",
+                    "711"
+                 ],
+                 correctAnswerIndex: 3)
+    ]
+}
---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Brody on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
